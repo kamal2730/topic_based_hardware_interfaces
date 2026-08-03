0998fe3e7cedbaa3d51c0cdc855070a64a5c734d
kamal2730 <itzkamalkant@gmail.com>
2026-08-03 17:02:17 +0530

    Add joint_command_topic_hardware_interface package

---
+++ joint_command_topic_hardware_interface/README.md
@@ -0,0 +1,42 @@
+# Joint Command Topic Based System
+
+The Joint Command Topic Based System implements a ros2_control `hardware_interface::SystemInterface` supporting command and state interfaces through the ROS topic communication layer. It publishes commands as `control_msgs/JointCommand` messages, grouped by interface type (position, velocity, effort).
+
+## ros2_control urdf tag
+
+The `joint_command_topic_hardware_interface` has a few `ros2_control` urdf tags to customize its behavior.
+
+### Parameters
+
+* joint_commands_topic: (default: "/robot_joint_command"). Example: `<param name="joint_commands_topic">/my_topic_joint_commands</param>`.
+* joint_states_topic: (default: "/robot_joint_states"). Example: `<param name="joint_states_topic">/my_topic_joint_states</param>`.
+* trigger_joint_command_threshold: (default: 1e-5). Used to avoid spamming the joint command topic when the difference between the current joint state and the joint command is smaller than this value, set to -1 to always send the joint command. Example: `<param name="trigger_joint_command_threshold">0.001</param>`.
+* sum_wrapped_joint_states: (default: "false"). Used to track the total rotation for joint states the values reported on the `joint_commands_topic` wrap from 2*pi to -2*pi when rotating in the positive direction. (Isaac Sim only reports joint states from 2*pi to -2*pi) Example: `<param name="sum_wrapped_joint_states">true</param>`.
+
+### Per-joint Parameters
+
+* mimic: Defined name of the joint to mimic. This is often used concept with parallel grippers. Example: `<param name="mimic">joint1</param>`.
+* multiplier: Multiplier of values for mimicking joint defined in mimic parameter. Example: `<param name="multiplier">-2</param>`.
+
+## Example
+
+```xml
+        <ros2_control name="name" type="system">
+            <hardware>
+              <plugin>joint_command_topic_hardware_interface/JointCommandTopicSystem</plugin>
+              <param name="joint_commands_topic">/topic_based_joint_commands</param>
+              <param name="joint_states_topic">/topic_based_joint_states</param>
+            </hardware>
+            <joint name="joint_1">
+                <command_interface name="position"/>
+                <command_interface name="velocity"/>
+                <state_interface name="position">
+                  <param name="initial_value">0.0</param>
+                </state_interface>
+                <state_interface name="velocity"/>
+            </joint>
+            ...
+        </ros2_control>
+```
+
+For each `write()` call, one `control_msgs/JointCommand` message is published per interface type (position, velocity, effort). Each message contains the joints that expose that command interface.
---
+++ joint_command_topic_hardware_interface/src/joint_command_topic_hardware_interface.cpp
@@ -0,0 +1,224 @@
+// Copyright 2026 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* Author: Kamalkant Thangaraju */
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include <angles/angles.h>
+#include <joint_command_topic_hardware_interface/joint_command_topic_hardware_interface.hpp>
+#include <rclcpp/executors.hpp>
+
+namespace
+{
+/** @brief Sums the total rotation for joint states that wrap from 2*pi to -2*pi
+when rotating in the positive direction */
+double sumRotationFromMinus2PiTo2Pi(const double current_wrapped_rad, double total_rotation_in)
+{
+  double delta = 0;
+  angles::shortest_angular_distance_with_large_limits(total_rotation_in, current_wrapped_rad, 2 * M_PI, -2 * M_PI,
+                                                      delta);
+
+  // Add the corrected delta to the total rotation
+  return total_rotation_in + delta;
+}
+}  // namespace
+
+namespace joint_command_topic_hardware_interface
+{
+
+CallbackReturn JointCommandTopicSystem::on_init(const hardware_interface::HardwareComponentInterfaceParams& params)
+{
+  if (hardware_interface::SystemInterface::on_init(params) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  const auto get_hardware_parameter = [this](const std::string& parameter_name, const std::string& default_value) {
+    if (auto it = get_hardware_info().hardware_parameters.find(parameter_name);
+        it != get_hardware_info().hardware_parameters.end())
+    {
+      return it->second;
+    }
+    return default_value;
+  };
+
+  if (auto it = get_hardware_info().hardware_parameters.find("trigger_joint_command_threshold");
+      it != get_hardware_info().hardware_parameters.end())
+  {
+    trigger_joint_command_threshold_ = std::stod(it->second);
+  }
+
+  topic_based_joint_states_subscriber_ = get_node()->create_subscription<sensor_msgs::msg::JointState>(
+      get_hardware_parameter("joint_states_topic", "/robot_joint_states"), rclcpp::SensorDataQoS(),
+      [this](const sensor_msgs::msg::JointState::SharedPtr joint_state) { latest_joint_state_ = *joint_state; });
+
+  topic_based_joint_command_publisher_ = get_node()->create_publisher<control_msgs::msg::JointCommand>(
+      get_hardware_parameter("joint_commands_topic", "/robot_joint_commands"), rclcpp::QoS(1));
+
+  // if the values on the `joint_states_topic` are wrapped between -2*pi and 2*pi (like they are in Isaac Sim)
+  // sum the total joint rotation returned on the `joint_state_values_` interface
+  if (get_hardware_parameter("sum_wrapped_joint_states", "false") == "true")
+  {
+    sum_wrapped_joint_states_ = true;
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::return_type JointCommandTopicSystem::read(const rclcpp::Time& /*time*/,
+                                                              const rclcpp::Duration& /*period*/)
+{
+  const auto& joints = get_hardware_info().joints;
+  for (std::size_t i = 0; i < latest_joint_state_.name.size(); ++i)
+  {
+    const auto it = std::find_if(joints.begin(), joints.end(),
+                                 [name = latest_joint_state_.name[i]](const hardware_interface::ComponentInfo& joint) {
+                                   return joint.name == name;
+                                 });
+    if (it != joints.end())
+    {
+      if (std::find_if(get_hardware_info().mimic_joints.begin(), get_hardware_info().mimic_joints.end(),
+                       [idx = static_cast<std::size_t>(std::distance(joints.begin(), it))](
+                           const hardware_interface::MimicJoint& mimic_joint) {
+                         return idx == mimic_joint.joint_index;
+                       }) != get_hardware_info().mimic_joints.end())
+      {
+        // mimic joints are updated at the end of this function
+        continue;
+      }
+
+      if (!latest_joint_state_.position.empty() && std::isfinite(latest_joint_state_.position.at(i)))
+      {
+        if (sum_wrapped_joint_states_)
+        {
+          auto name = latest_joint_state_.name[i] + "/" + hardware_interface::HW_IF_POSITION;
+
+          set_state(name, sumRotationFromMinus2PiTo2Pi(latest_joint_state_.position.at(i), get_state(name)));
+        }
+        else
+        {
+          set_state(latest_joint_state_.name[i] + "/" + hardware_interface::HW_IF_POSITION,
+                    latest_joint_state_.position.at(i));
+        }
+      }
+      if (!latest_joint_state_.velocity.empty() && std::isfinite(latest_joint_state_.velocity.at(i)))
+      {
+        set_state(latest_joint_state_.name[i] + "/" + hardware_interface::HW_IF_VELOCITY,
+                  latest_joint_state_.velocity.at(i));
+      }
+      if (!latest_joint_state_.effort.empty() && std::isfinite(latest_joint_state_.effort.at(i)))
+      {
+        set_state(latest_joint_state_.name[i] + "/" + hardware_interface::HW_IF_EFFORT,
+                  latest_joint_state_.effort.at(i));
+      }
+    }
+  }
+
+  // Update mimic joints
+  for (const auto& mimic_joint : get_hardware_info().mimic_joints)
+  {
+    const auto& mimic_joint_name = joints.at(mimic_joint.joint_index).name;
+    const auto& mimicked_joint_name = joints.at(mimic_joint.mimicked_joint_index).name;
+    if (has_state(mimic_joint_name + "/" + hardware_interface::HW_IF_POSITION))
+    {
+      set_state(mimic_joint_name + "/" + hardware_interface::HW_IF_POSITION,
+                mimic_joint.offset +
+                    mimic_joint.multiplier * get_state(mimicked_joint_name + "/" + hardware_interface::HW_IF_POSITION));
+    }
+    if (has_state(mimic_joint_name + "/" + hardware_interface::HW_IF_VELOCITY))
+    {
+      set_state(mimic_joint_name + "/" + hardware_interface::HW_IF_VELOCITY,
+                mimic_joint.multiplier * get_state(mimicked_joint_name + "/" + hardware_interface::HW_IF_VELOCITY));
+    }
+    if (has_state(mimic_joint_name + "/" + hardware_interface::HW_IF_ACCELERATION))
+    {
+      set_state(mimic_joint_name + "/" + hardware_interface::HW_IF_ACCELERATION,
+                mimic_joint.multiplier * get_state(mimicked_joint_name + "/" + hardware_interface::HW_IF_ACCELERATION));
+    }
+  }
+
+  return hardware_interface::return_type::OK;
+}
+
+hardware_interface::return_type JointCommandTopicSystem::write(const rclcpp::Time& /*time*/,
+                                                               const rclcpp::Duration& /*period*/)
+{
+  const auto& joints = get_hardware_info().joints;
+  // To avoid spamming the joint command topic we check the difference between the joint states and
+  // the current joint commands, if it's smaller than a threshold we don't publish it.
+  auto diff = 0.0;
+  for (std::size_t i = 0; i < joints.size(); ++i)
+  {
+    for (const auto& interface : joints[i].command_interfaces)
+    {
+      const bool supported_command_interface = interface.name == hardware_interface::HW_IF_POSITION ||
+                                               interface.name == hardware_interface::HW_IF_VELOCITY ||
+                                               interface.name == hardware_interface::HW_IF_EFFORT;
+      if (!supported_command_interface)
+      {
+        continue;
+      }
+      // sum the absolute difference for all joints
+      diff += std::abs(get_state(joints[i].name + "/" + interface.name) -
+                       get_command(joints[i].name + "/" + interface.name));
+    }
+  }
+  if (diff <= trigger_joint_command_threshold_)
+  {
+    return hardware_interface::return_type::OK;
+  }
+
+  std::map<std::string, control_msgs::msg::JointCommand> commands_by_interface;
+  for (std::size_t i = 0; i < joints.size(); ++i)
+  {
+    for (const auto& interface : joints[i].command_interfaces)
+    {
+      const bool supported_command_interface = interface.name == hardware_interface::HW_IF_POSITION ||
+                                               interface.name == hardware_interface::HW_IF_VELOCITY ||
+                                               interface.name == hardware_interface::HW_IF_EFFORT;
+      if (!supported_command_interface)
+      {
+        continue;
+      }
+      if (commands_by_interface.find(interface.name) == commands_by_interface.end())
+      {
+        commands_by_interface[interface.name].header.stamp = get_node()->now();
+        commands_by_interface[interface.name].interface_name = interface.name;
+      }
+      commands_by_interface[interface.name].joint_names.push_back(joints[i].name);
+      commands_by_interface[interface.name].values.push_back(get_command(joints[i].name + "/" + interface.name));
+    }
+  }
+  if (rclcpp::ok())
+  {
+    for (auto& [interface_name, msg] : commands_by_interface)
+    {
+      topic_based_joint_command_publisher_->publish(msg);
+    }
+  }
+
+  return hardware_interface::return_type::OK;
+}
+}  // end namespace joint_command_topic_hardware_interface
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(joint_command_topic_hardware_interface::JointCommandTopicSystem,
+                       hardware_interface::SystemInterface)
